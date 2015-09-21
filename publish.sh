1f95132bd1a3eabce883d35130d639c0b5056630
Yusen <rainforest92@126.com>
2015-09-21 19:09:15 +0800

    [changed] publish.sh

--- publish.sh
+++ publish.sh
@@ -2,18 +2,27 @@
 
 gulp bower
 gulp page
-# git commit -am 
-npm version $1 #/git commit -m '0.1.0'
-npm publish
-git push origin v$1
+# don't use npm version $1
+modify package.json
+git add -A .
+git commit -m $1
 git checkout master
 git merge develop
 git push --all
+git tag $1
+git push origin $1
+npm publish
 cd ../regular-ui-bower
-git commit -am $1
+git add -A .
+git commit -m $1
 git push
+git tag $1
+git push origin $1
 cd ../regular-ui.github.io
-git commit -am $1
+git add -A .
+git commit -m $1
 git push
+git tag $1
+git push origin $1
 cd ../regular-ui
 git checkout develop
